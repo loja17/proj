99e82ad751f77a5e3e9ee6c3a6899231145a5c94
Louise Jakobsson <louise_jakobsson@hotmail.com>
2017-11-19 21:09:52 +0000

    Prepare to tag version

--- config/navbar.php
+++ config/navbar.php
@@ -37,7 +37,7 @@ return [
 
 
 
-/*
+
     // Used as menu together with responsive menu
     // Name of this menu
     "navbarMax" => [
@@ -50,20 +50,26 @@ return [
         "items" => [
 
             "report" => [
-                "text"  => t("Report"),
+                "text"  => t("Redovisning"),
                 "url"   => $this->di->get("url")->create("report"),
-                "title" => t("Reports from kmom assignments"),
+                "title" => t("Redovisning av kursmoment"),
                 "mark-if-parent" => true,
             ],
 
             "about" => [
-                "text"  => t("About"),
+                "text"  => t("Om"),
                 "url"   => $this->di->get("url")->create("about"),
-                "title" => t("About this website")
+                "title" => t("Om denna webbsida")
+            ],
+
+            "test" => [
+                "text"  => t("Test"),
+                "url"   => $this->di->get("url")->create("test"),
+                "title" => t("Testsida")
             ],
         ],
     ],
-*/
+
 
 
     /**
--- content/block/byline.md
+++ content/block/byline.md
@@ -1,6 +1,6 @@
-<div style="border: 1px dotted;display: block;width: 65%;margin-left: 9%;margin-right: -9%; font-size: 13px; height: 85px;">
+<div>	
 	
-	<img src="img/cookie.jpg" alt="Kråkan" style="padding: 2px;margin: 2px;bottom: 2px;height: 80px;width: 80px;position:relative;float: left;">
+	<img src="img/cookie.jpg" alt="Kråkan" style="padding: 2px;margin: 2px;bottom: 2px;height: 80px;width: 80px;position:relative;float: left;border-radius: 50%;">
     Louise Jakobsson har en kandidatexamen i Informatik från Göteborgs 
     Universitet men vill bredda sina kunskaper inom webbprogrammering, 
     läser för närvarande kursen Teknisk webbdesign och användbarhet som ingår i
